4d30160cbc14602cca12ef6bfc01b310764f08af
Julius Canales <juliuscanales118@gmail.com>
2022-03-31 20:07:09 -0700

    panic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-  // Setting  possible characters in a password.
-  var charsPossible = "qwertyuiopasdfghjklzxcvbnm QWERTYUIOPASDFGHJKLZXCVBNM 1234567890 ~!@#$%^&*()_-+="
-  // This sets the maximum amount of characters in a password to 12.
-  var passwordLength = 12
+  // Possible characters in a password: "qwertyuiopasdfghjklzxcvbnm QWERTYUIOPASDFGHJKLZXCVBNM 1234567890 ~!@#$%^&*()_-+="
+  //These are lists of possible characters.
+  var charactersLowCase = "qwertyuiopasdfghjklzxcvbnm"
+  var charactersUpCase = "QWERTYUIOPASDFGHJKLZXCVBNM"
+  var charactersSpecial = "~!@#$%^&*()_-+="
+  var charactersNum = "1234567890"
 
 // Write password to the #password input
 function writePassword() {
+  // 
+  var askUserPasswordLength = prompt("Select a length for the password. It can anywhere from 8 to 128 characters.") 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -14,12 +18,20 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
-for (var i = 0; i <= passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random() * charsPossible.length);
-  password += charsPossible.substring(randomNumber, randomNumber +1);
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * charsPossible.length);
+    password += charsPossible.substring(randomNumber, randomNumber +1);
  }
 
- console.log(password)
+ function writePassword(password){
+  var password = generatePassword();
+  var pwTextArea = document.getElementById("#password");
+  pwTextArea.value = password;
+  return ("")
+  
+  }
+ console.log(generateBtn) 
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
