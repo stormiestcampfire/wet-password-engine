4df8980cb08765832e478b25e5ebdcc9c20893dd
Julius Canales <juliuscanales118@gmail.com>
2022-03-31 19:10:29 -0700

    add for loop and password limits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+  // Setting  possible characters in a password.
+  var charsPossible = "qwertyuiopasdfghjklzxcvbnm QWERTYUIOPASDFGHJKLZXCVBNM 1234567890 ~!@#$%^&*()_-+="
+  // This sets the maximum amount of characters in a password to 12.
+  var passwordLength = 12
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +16,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+for (var i = 0; i <= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * charsPossible.length);
+  password += charsPossible.substring(randomNumber, randomNumber +1);
+ }
+
+ console.log(password)
