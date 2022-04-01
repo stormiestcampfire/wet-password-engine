0447adb0656551d47fe491f44fcb197c73ff4a38
Julius Canales <juliuscanales118@gmail.com>
2022-03-31 21:35:03 -0700

    finishing mechanism for generating password

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,13 +13,33 @@ function writePassword() {
   var passwordCharacters = "";
   // 
   var password = generatePassword();
-  var askUserPasswordLength = prompt("Select a length for the password. It can anywhere from 8 to 128 characters.") 
+    alert("Please select from the criteria following criteria for your password.")
+    
+    var askUserPasswordLength = prompt("Select a length for the password. It can anywhere from 8 to 128 characters.") 
+    
+  // asks user if they would like to add lowercase letters
+    var askUserLowCase = confirm("Mix in lowercase letters?")
+      if (askUserLowCase) {
+      passwordCharacters += askUserLowCase;
+      }
+  // asks user if they would like to add uppercase letters
+    var askUserUpCase = confirm("Mix in uppercase letters?");
+      if (askUserUpCase) {
+      passwordCharacters += askUserUpCase;
+      }
+  // asks user if they would like to add special characters
+      var askUserSpecCase = confirm("Mix in special characters?");
+      if (askUserSpecCase) {
+        passwordCharacters += askUserSpecCase;
+      }
+  // asks user if they would like to add numbers to the password
+      var askUserNum = confirm("Mix in numbers?");
+      if (askUserNum) {
+        passwordCharacters += askUserNum;
+      
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
-
 
   for (var i = 0; i <= askUserpasswordLength; i++) {
     var randomNumber = Math.floor(Math.random() * charsPossible.length);
